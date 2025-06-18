ab8f5bfcf33a0da585ab867a5abcd5098426e0ef
Prabesh Bhujel <107237006+pbhujel03@users.noreply.github.com>
2025-06-18 22:09:23 +0545

    Update conn.php

--- DatabaseConn/conn.php
+++ DatabaseConn/conn.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
+// Start session only if not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 $host = "localhost";     
 $dbname = "Rent_Ease";    
@@ -8,8 +11,8 @@ $password = "";
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
+// Check for connection error
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-//echo "conn suss";
 ?>
